ab6ab74df4032a01e8340d95245b342f7d2a329c
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-03-28 19:33:12 -0700

    Send request to flickr api

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org' do
+  gem 'faraday'
   gem 'sinatra'
+
+  group :test, :development do
+    gem 'dotenv'
+  end
 end
--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'faraday'
+require 'dotenv/load'
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = ENV['ORIGIN']
+end
 
 get '/' do
-  puts 'hello world'
+  query = params[:query]
+  num = params[:num] || 50
+  page = params[:page] || 1
+
+  url = 'https://api.flickr.com/services/rest/?' \
+        "api_key=#{ENV['API_KEY']}&" \
+        'method=flickr.photos.search&' \
+        'format=json&' \
+        'nojsoncallback=1&' \
+        "per_page=#{num}&" \
+        "page=#{page}&" \
+        "text=#{query}"
+
+  response = Faraday.get(url)
+  response.body
 end
